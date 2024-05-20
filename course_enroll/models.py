45add1cf1cc162c058681cd33bb77cd25f9412ba
DavidQF555 <davidfeng2003@gmail.com>
2024-05-20 12:17:55 -0700

    modified models

--- course_enroll/models.py
+++ course_enroll/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
     date_added = models.DateTimeField()
     capacity = models.IntegerField()
-    enrolled = models.IntegerField(default=0)
 
 class Student(models.Model):
-    name = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     courses = models.ManyToManyField(Course)
