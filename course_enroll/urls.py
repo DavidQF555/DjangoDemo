3c5781214ac0ebf7a1b4934b65d496b5bcfe78c6
DavidQF555 <davidfeng2003@gmail.com>
2024-05-20 14:58:05 -0700

    improved views

--- course_enroll/urls.py
+++ course_enroll/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = "course_enroll"
 urlpatterns = [
     path("", views.enroll, name="enroll"),
-    path("courses/<int:id>", views.courses, name="courses")
+    path("<int:page>", views.enroll, name="enroll_page"),
+    path("courses/<int:id>", views.courses, name="courses"),
+    path("courses/<int:id>/register", views.register, name="register_course")
 ]
--- course_enroll/views.py
+++ course_enroll/views.py
@@ -1,10 +1,26 @@
-from django.http import HttpResponse, Http404
+from math import ceil
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import Course
 
-def enroll(request):
-    return render(request, "enroll.html")
+page_limit = 25
+
+def enroll(request, page=1):
+    if page <= 0:
+        return HttpResponseRedirect(reverse('course_enroll:enroll'))
+    pages = ceil(Course.objects.count() / page_limit)
+    if page > pages:
+        return HttpResponseRedirect(reverse('course_enroll:enroll_page', args=(pages,)))
+    all = Course.objects.order_by('name')
+    courses = all[(page - 1) * page_limit : page * page_limit]
+    params = { 'courses': courses }
+    if page > 1:
+        params['prev'] = page - 1
+    if page < pages:
+        params['next'] = page + 1
+    return render(request, "enroll.html", params)
 
 def courses(request, id):
     try:
@@ -12,3 +28,6 @@ def courses(request, id):
     except Course.DoesNotExist:
         raise Http404("Course does not exist")
     return render(request, "courses.html", { "course": course })
+
+def register(request, id):
+    pass
