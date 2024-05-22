ebd4d6d8c304d497c78afcdedc7331efec5a0694
DavidQF555 <davidfeng2003@gmail.com>
2024-05-22 14:35:55 -0700

    basic authentication

--- course_enroll/urls.py
+++ course_enroll/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("", views.enroll, name="enroll"),
     path("<int:page>", views.enroll, name="enroll_page"),
     path("courses/<int:id>", views.courses, name="courses"),
-    path("courses/<int:id>/register", views.register, name="register_course")
+    path("courses/<int:id>/register", views.register, name="register_course"),
+    path("accounts/login", views.account_login, name="login"),
+    path("accounts/register", views.account_register, name="register"),
+    path("accounts/logout", views.logout, name="logout")
 ]
--- course_enroll/views.py
+++ course_enroll/views.py
@@ -1,9 +1,11 @@
 from math import ceil
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-
-from .models import Course
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout as lo
+from django.contrib import messages
+from .models import Course, Student
 
 page_limit = 25
 
@@ -20,6 +22,7 @@ def enroll(request, page=1):
         params['prev'] = page - 1
     if page < pages:
         params['next'] = page + 1
+    params['auth'] = request.user.is_authenticated
     return render(request, "enroll.html", params)
 
 def courses(request, id):
@@ -27,7 +30,49 @@ def courses(request, id):
         course = Course.objects.get(pk=id)
     except Course.DoesNotExist:
         raise Http404("Course does not exist")
-    return render(request, "courses.html", { "course": course })
+    count = Student.objects.filter(courses__in=[id]).count()
+    return render(request, "courses.html", { "course": course, "count": count, 'auth': request.user.is_authenticated })
 
 def register(request, id):
     pass
+
+def account_login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if not username or not password:
+            messages.add_message(request, messages.ERROR, "Please fill out all fields.")
+            return HttpResponseRedirect(reverse("course_enroll:login"))
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            messages.add_message(request, messages.ERROR, "Invalid login.")
+            return HttpResponseRedirect(reverse("course_enroll:login"))
+        login(request=request, user=user)
+        messages.add_message(request, messages.SUCCESS, "Successfully logged in.")
+        return HttpResponseRedirect(reverse("course_enroll:enroll"))
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("course_enroll:enroll"))
+    return render(request, "accounts/login.html")
+
+def account_register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        if not username or not password:
+            messages.add_message(request, messages.ERROR, "Please fill out all fields.")
+            return HttpResponseRedirect(reverse("course_enroll:register"))
+        if User.objects.filter(username=username).exists():
+            messages.add_message(request, messages.ERROR, "There is already a user with the same username.")
+            return HttpResponseRedirect(reverse("course_enroll:register"))
+        user = User.objects.create_user(username=username, password=password)
+        user.save()
+        messages.add_message(request, messages.SUCCESS, "Successfully registered. Please login.")
+        return HttpResponseRedirect(reverse("course_enroll:register"))
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("course_enroll:enroll"))
+    return render(request, "accounts/register.html")
+
+def logout(request):
+    lo(request)
+    messages.add_message(request, messages.SUCCESS, "Successfully logged out.")
+    return HttpResponseRedirect(reverse("course_enroll:enroll"))
